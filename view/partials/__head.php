0ebda5d5d8981133ca7c727d00a52c0993fc9a11
MD Tafhim Faisal <tropical10tafhim@gmail.com>
2020-02-05 01:33:08 +0600

    landing page

--- view/partials/__head.php
+++ view/partials/__head.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-  <title>Logix Automations</title>
+  <title>Al Sahara</title>
   <link rel="stylesheet" href="http://<?=$_SERVER['HTTP_HOST']?>/plugins/fontawesome-free/css/all.min.css">
   <link rel="stylesheet" href="http://<?=$_SERVER['HTTP_HOST']?>/dist/css/adminlte.min.css">
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
