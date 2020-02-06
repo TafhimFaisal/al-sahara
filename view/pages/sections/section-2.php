bc563b8dbf48bcaf30dfb4b3d2b458889377b78c
MD Tafhim Faisal <tropical10tafhim@gmail.com>
2020-02-06 08:44:40 +0600

    responsive

--- view/pages/sections/section-2.php
+++ view/pages/sections/section-2.php
@@ -9,32 +9,32 @@
             </div>
             <div class="col-md-4 section-2-box text-center">
                 <div class="inside-box">
-                    <h3>Reliable Manpower Supply</h3>
-                    <p>We assure you to provide the best manpower supply services for your business and will never let you down.</p>
+                    <h3>On Time Service</h3>
+                    <p>We supply the right manpower as per our clients requirements in a timely manner.</p>
                 </div>
             </div>
             <div class="col-md-4 section-2-box text-center">
                 <div class="inside-box">
-                    <h3>Reliable Manpower Supply</h3>
-                    <p>We assure you to provide the best manpower supply services for your business and will never let you down.</p>
+                    <h3>Negotiable Rates</h3>
+                    <p>Provides skilled manpower supply at negotiable hourly or monthly rates which is the best in industry.</p>
                 </div>
             </div>
             <div class="col-md-4 section-2-box text-center">
                 <div class="inside-box">
-                    <h3>Reliable Manpower Supply</h3>
-                    <p>We assure you to provide the best manpower supply services for your business and will never let you down.</p>
+                    <h3>Flexible Contracts</h3>
+                    <p>We provide a team or individuals who can meet your manpower requirements perfectly in short term and long term basis.</p>
                 </div>
             </div>
             <div class="col-md-4 section-2-box text-center">
                 <div class="inside-box">
-                    <h3>Reliable Manpower Supply</h3>
-                    <p>We assure you to provide the best manpower supply services for your business and will never let you down.</p>
+                    <h3>Industry Expertise</h3>
+                    <p>With more than 15 years of expertise in manpower supply services in UAE , we understand the manpower requirements of the clients in a better way.</p>
                 </div>
             </div>
             <div class="col-md-4 section-2-box text-center">
                 <div class="inside-box">
-                    <h3>Reliable Manpower Supply</h3>
-                    <p>We assure you to provide the best manpower supply services for your business and will never let you down.</p>
+                    <h3>Health and Safety</h3>
+                    <p>Our personnel are trained to the industry standard and there by contribute to a zero harm work place.</p>
                 </div>
             </div>
         </div>
