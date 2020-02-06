2a809764a45c6c7995027f79301a52e3b19c19c3
MD Tafhim Faisal <tropical10tafhim@gmail.com>
2020-02-06 07:30:23 +0600

    access-controll page and cctv page

---
+++ view/pages/man-power-supply/cctv.php
@@ -0,0 +1,42 @@
+<div class="container">
+    <div class="cctv">
+        <h1> CCTV Services </h1>
+        <p>
+            We are committed to give the customers a seamless experience in installation,
+            configuration, maintenance and upkeep, upgradation, repairs and more for their electronic security, surveillance equipment and robust cabling network. 
+        </p>
+
+
+        <h1> Supply & Installation </h1>
+        <p>
+            As a professional security and surveillance system providers, we have the best in class systems and cabling network for every need and every budget. We offer complete handover and training on installation of the systems and network.
+        </p>
+
+        <h1> Maintenance Contracts </h1>
+        <p>
+            We offer comprehensive maintenance contracts for our clients that include preventive maintenance,       
+            regular repairs, system expansion, upgrades and more of both your hardware and software systems. Since we are conveniently located in the heart of UAE, our team will reach you in record time so that you enjoy minimal downtime.
+        </p>
+         
+        <h1> Project Management & Consultancy </h1>
+        <p>
+            We offer 360° security solutions using proactive approach to give our clients total security from both manmade and natural hazards. Our experts will design security and network solutions according to the nature, size and unique requirements of your business.
+        </p>
+
+        <h1>
+            Our Product Categories:
+        </h1>
+        <ul>
+            <li>CCTV & SURVEILLANCE</li>
+            <li>ACCESS CONTROL & TIME ATTENDANCE</li>
+            <li>IP TELEPHONY & PABX</li>
+            <li>AUDIO / VIDEO INTERCOM</li>
+            <li>STRUCTURED CABLING</li>
+        </ul>
+
+        <h1> CCTV & SURVEILLANCE </h1>
+        <p>
+            AL SAHARA is a leading service provider for ALL types of CCTV and Surveillance systems. We have entire range of products and brands compatible for use in crime prevention, traffic monitoring, retail outlets,    industrial processes, corporate houses and home security. Our cutting edge surveillance and security systems come with remote viewing to curb the chances of theft, burglary and keep a tab on day to day activities of your employees. We offer end to end service on CCTV and surveillance equipment, right from choosing the right equipment to suit the size and nature of business to installation, configuration and maintenance of the systems.
+        </p>
+    </div>
+</div>
