f5c30998d54ec26453ae1a21501b05c37e0f7c76
MD Tafhim Faisal <tropical10tafhim@gmail.com>
2020-02-06 06:42:19 +0600

    services page

--- view/pages/why-choose-us.php
+++ view/pages/why-choose-us.php
@@ -1,5 +1,21 @@
-<h1>
+<div class="container">
+    <div class="why-choose-us">
 
-this is wht choose us page
-
-</h1>
+        <h1>REASON TO CHOOSE US AS YOUR MANPOWER SUPPLY COMPANY</h1>
+        <p>
+            All businesses require talented manpower resources to execute the projects on time. But it is a costly and time-consuming affair to hire such skilled and talented manpower for companies. Here comes the role of Manpower Supply Company that provides quick and easy access to the best workers to companies of different sectors. We offer a swift and easy transition for the companies who are in need of qualified manpower. 
+            We are one of the most prominent manpower supply companies in UAE. We offer premium quality labor supply services in all the emirates of the UAE. We at Manpower Supply provide skilled and unskilled manpower to different companies of various sectors for the Long-Term as well as Short-Term.
+            
+            There are different kinds of skills to execute a wide range of   industries. Each of the jobs needs special skills and experience to complete the job professionally. We do provide manpower supply services to the following industries in UAE: 
+        </p>
+        <ul>
+            <li>Civil - Construction Industry </li>
+            <li> Mechanical, Electrical and Plumbing – (MEP) </li>
+            <li> Hospitality (Catering / Cleaning) </li>
+        </ul>
+        <p>
+            The Manpower we provide is skilled, Trained and certified according to the Industry Standards. They are committed to the health and safety and execute the work without any chance of accidents. It is hard to get the skilled workforce but we can provide you with the pool of skilled and talented workforce at affordable rates. 
+            We have the experience to provide a different kind of  manpower to different companies. So whatever sector your company belongs to, then we can provide you with the best and skilled       workforce in a short time in a timely manner.
+        </p>
+    </div>
+</div>
