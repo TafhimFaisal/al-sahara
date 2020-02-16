f7173d391256ef743e0d11f52b8cd73dacadcb4e
MD Tafhim Faisal <tropical10tafhim@gmail.com>
2020-02-17 02:19:52 +0600

    email

--- email/email.php
+++ email/email.php
@@ -1,14 +1,12 @@
 <?php
 
     // $to = 'Info@sahara-uae.com';
-    $to = '2195a43808-f86dbb@inbox.mailtrap.io';
     $subject = 'sahara-uae';
-    $text  = 'Name: '.$_POST['name'].' '.'Phone:'.$_POST['phone'].' Massage : '.$_POST['massage'];
+    $text   =   'Name:'.$_POST['name'].
+                'Phone :'.$_POST['phone'].
+                'Massage :'.$_POST['massage'];
     $header= $_POST['email'];
 
     mail($to,$subject,$text,$header);
     header('Location:http://'.$_SERVER["HTTP_HOST"].'/contact-us.php');
-    
-    // https://www.youtube.com/watch?v=4q0gYjAVonI
-    //https://www.youtube.com/watch?v=No8gaqsFdMw
 ?>
