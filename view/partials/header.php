39949e736c7d1f54b4a439be95a0afaf1d9536a6
MD Tafhim Faisal <tropical10tafhim@gmail.com>
2020-03-25 17:39:57 +0600

    head change

--- view/partials/header.php
+++ view/partials/header.php
@@ -8,7 +8,7 @@
                 <img class="mt-3" src="http://<?=$_SERVER['HTTP_HOST']?>/Images/logos/logo-final.jpeg" height="100px" width="100px" alt="" srcset="">
             </div>
             
-            <div class="col-10 contact-info">
+            <div class="col-10 contact-info mt-2">
 
                 <div class="social-midia-icons">
                     <i class="fab fa-facebook-square"></i>
@@ -16,17 +16,30 @@
                     <i class="fab fa-twitter-square"></i>
                 </div>
                 
-                <div class="contact-info-header">
-                    
-                    <div class="phone">
-                        <div class="phone-icon"><i class="fa fa-phone" aria-hidden="true"></i></div>
-                        <p>+97165322336</p>
+                <div class="contact-info-header mt-4">
+
+                    <div class="row">
+                        <div class="col-md-2"></div>
+                        <div class="col-md-4">
+                            <div class="phone">
+                                <div class="phone-icon"><i class="fa fa-phone" aria-hidden="true"></i></div>
+                                <p>+97165322336</p>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="email">
+                                <div class="email-icon"><i class="fa fa-envelope" aria-hidden="true"></i></div>
+                                <p> Info@sahara-uae.com </p>
+                            </div>
+                        </div>
+
+                        <div class="col-md-2">
+                            <a role='button' href="http://<?=$_SERVER['HTTP_HOST']?>/contact-us.php" type="button" class="btn btn-dark btn-md text-white rounded-pill">contuct us</a>
+                        </div>
+
                     </div>
-                
-                    <div class="email">
-                        <div class="email-icon"><i class="fa fa-envelope" aria-hidden="true"></i></div>
-                        <p> Info@sahara-uae.com </p>
-                    </div>
+
                 </div>
 
             </div>
