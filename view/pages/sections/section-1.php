41821c393c928fab1dba12182fb7b66e26a8dd7f
MD Tafhim Faisal <tropical10tafhim@gmail.com>
2020-03-28 03:15:51 +0600

    slider

--- view/pages/sections/section-1.php
+++ view/pages/sections/section-1.php
@@ -3,9 +3,9 @@
         <div class="col-md-6" style="margin-top:60px">
             <h2> Our Manpower Supply Service Specialties </h2>
             <p>
-                We are the right Labor Supply Partner to meet your business goal.
-                Ensure your success with our expertise in Manpower Supply Services.
-                We are the ultimate destination for your manpower supply requirements in UAE.
+                We are the right Labor Supply Partner to meet your business goal. Ensure your success
+                with our expertise in Manpower Supply Services. We are the ultimate destination for
+                your manpower supply requirements in UAE.
             </p>
         </div>
         <div class="col-md-6">
--- view/partials/slider.php
+++ view/partials/slider.php
@@ -3,20 +3,28 @@
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="http://<?=$_SERVER['HTTP_HOST']?>/Images/slider/slider-12.jpeg" height="600px" class="d-block w-100" alt="...">
+      <img src="http://<?=$_SERVER['HTTP_HOST']?>/Images/work8.jpg" height="600px" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block mb-5">
+        <h2 class="text-light">Professional Manpower Supply</h2>
+        <p> <a role='button' href="http://<?=$_SERVER['HTTP_HOST']?>/contact-us.php" type="button" class="btn btn-outline-dark btn-lg rounded-pill">Contact Us</a> </p>
+      </div>
     </div>
     <div class="carousel-item">
-      <img src="http://<?=$_SERVER['HTTP_HOST']?>/Images/slider/slider-13.jpeg" height="600px" class="d-block w-100" alt="...">
+      <img src="http://<?=$_SERVER['HTTP_HOST']?>/Images/Work9.jpg" height="600px" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block mb-5">
+        <h2 class="text-dark">Your Trusted Contracting Partner</h2>
+        <p> <a role='button' href="http://<?=$_SERVER['HTTP_HOST']?>/contact-us.php" type="button" class="btn btn-outline-dark btn-lg rounded-pill">Contact Us</a> </p>
+      </div>
     </div>
     <div class="carousel-item">
-      <img src="http://<?=$_SERVER['HTTP_HOST']?>/Images/slider/slider-8.jpeg" height="600px" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="http://<?=$_SERVER['HTTP_HOST']?>/Images/slider/slider-10.jpeg" height="600px" class="d-block w-100" alt="...">
+      <img src="http://<?=$_SERVER['HTTP_HOST']?>/Images/work5.jpg" height="600px" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block mb-5">
+        <h2 class="text-dark">Valuing your Time & Money</h2>
+        <p> <a role='button' href="http://<?=$_SERVER['HTTP_HOST']?>/contact-us.php" type="button" class="btn btn-outline-dark btn-lg rounded-pill">Contact Us</a> </p>
+      </div>
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
