8db800f022079818999e48effad4a00982e4a8fb
MD Tafhim Faisal <tropical10tafhim@gmail.com>
2020-04-01 17:27:09 +0600

    blog

--- view/pages/blogs/blog-2.php
+++ view/pages/blogs/blog-2.php
@@ -1,13 +1,13 @@
 <div class="container">
     <h2>
-        MANPOWER AGENCIES
+        MANPOWER DEVELOPMENT
     </h2>
     <small>
-        <div class="date"> FEB 17, 2019 </div>
+        <div class="date"> JUNE 17, 2019 </div>
         <div class="writer"> ADMIN- AL SAHARA MANPOWER SUPPLY </div>
     </small>
     <img src="http://<?=$_SERVER['HTTP_HOST']?>/Images/blog/blog-2.jpg" width='100%' height="100%" alt="" srcset="">
     <p>
-        The Manpower Agencies all over the world having the same ethics, i.e. giving their clients the best support of Manpower with skill and and experiences. UAE is one of the country of Opportunities and Talents. So, the agencies here are mostly looking for skilled and experienced Labours and Executives with better salary package. 
+        We have a recruiting system that brings maximum positive outcome to companies when they recruit employees.  Being the best job consultancy for the Middle East jobs , Al Sahara Manpower Services has the team and resources to recruit the right candidate across multiple industries in and around the Middle East region. As a manpower recruiter
     </p>
 </div>
--- view/pages/blogs/blog-3.php
+++ view/pages/blogs/blog-3.php
@@ -1,13 +1,13 @@
 <div class="container">
     <h2>
-        MANPOWER AGENCIES
+        IMPORTANCE OF MANPOWER
     </h2>
     <small>
-        <div class="date"> FEB 17, 2019 </div>
+        <div class="date"> FEBRUARY 14, 2020 </div>
         <div class="writer"> ADMIN- AL SAHARA MANPOWER SUPPLY </div>
     </small>
     <img src="http://<?=$_SERVER['HTTP_HOST']?>/Images/blog/blog-3.jpg" width='100%' height="100%" alt="" srcset="">
     <p>
-        The Manpower Agencies all over the world having the same ethics, i.e. giving their clients the best support of Manpower with skill and and experiences. UAE is one of the country of Opportunities and Talents. So, the agencies here are mostly looking for skilled and experienced Labours and Executives with better salary package. 
+        Manpower affects everything in a business from production to client relationships. Without adequate and supportive manpower, a business will never be successful. The staff should be well trained on their tasks while managers should know how to lead.  Companies can establish and maintain good rapport with their customers if they can ensure good manpower
     </p>
 </div>
